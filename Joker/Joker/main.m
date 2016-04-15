a9e099f704ec010b480745f1599ddb3a8fb907d6
ACFancy <pgwizardlv@gmail.com>
2016-04-15 09:21:01 +0800

    Project Test Start

---
+++ Joker/Joker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Joker
+//
+//  Created by PGWizardMac on 16/4/15.
+//  Copyright © 2016年 PGWizardMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
